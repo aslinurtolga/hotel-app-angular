e96876fd23747d58651e8af2757436563c8ff77f
AslinurTolga <aslinurtolga@gmail.com>
2024-11-17 20:01:05 +0300

    feat: created reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservationService {
+  private reservations: Reservation[] = [];
+  // CRUD
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
 
-  constructor() { }
+  // read
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find((res) => res.id === id);
+  }
+
+  // create
+  addReservations(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+  // update
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex((res) => res.id === id);
+    this.reservations.splice(index, 1);
+  }
+  // update
+  updateReservation(updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(
+      (res) => res.id === updatedReservation.id
+    );
+    this.reservations[index] = updatedReservation;
+  }
 }
